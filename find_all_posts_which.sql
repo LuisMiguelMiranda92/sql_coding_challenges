cdb879fae49b0216ab08ed2b53d30da35e5dcf78
Miguel Miranda <lmbasmiranda@gmail.com>
2025-09-08 12:07:19 -0700

    added new challenge

---
+++ find_all_posts_which.sql
@@ -0,0 +1,7 @@
+#Find all posts which were reacted to with a heart. For such posts output all columns from facebook_posts table.
+
+select posts.*
+FROM facebook_posts posts
+JOIN facebook_reactions reactions
+ON posts.post_id = reactions.post_id
+where reactions.reaction = 'heart'
