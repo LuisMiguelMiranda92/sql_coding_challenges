250d2b972441a760329cc1bcca0600e1f0fe5e87
Miguel Miranda <lmbasmiranda@gmail.com>
2025-09-29 13:25:50 -0700

    added new challenge

---
+++ monthly_active_users.sql
@@ -0,0 +1,11 @@
+/*
+Find the monthly active users for January 2021 for each account. Your output should have account_id and the monthly count for that account.
+
+
+*/
+select account_id,
+        COUNT(DISTINCT user_id) AS monthly_count
+from sf_events
+WHERE record_date >= '01/01/2021' 
+        AND record_date < '02/01/2021'
+GROUP BY account_id
---
+++ user_growth_rate.sql
@@ -0,0 +1,12 @@
+/*
+Find the growth rate of active users for Dec 2020 to Jan 2021 for each account. The growth rate is defined as the number of users in January 2021 divided by the number of users in Dec 2020. Output the account_id and growth rate.
+
+
+*/
+select account_id,
+        CAST(
+            COUNT(DISTINCT CASE WHEN record_date >= '2021-01-01' AND record_date < '2021-02-01' THEN user_id END) AS DECIMAL) / 
+            NULLIF(
+                COUNT(DISTINCT CASE WHEN record_date >= '2020-12-01' AND record_date < '2021-01-01' THEN user_id END),0) AS growth_rate
+from sf_events
+GROUP BY account_id
