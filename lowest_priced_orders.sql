8400c887d1e27d9df234aca9ed3162952f4ce1c9
Miguel Miranda <lmbasmiranda@gmail.com>
2025-11-18 09:59:02 -0800

    added new project

--- lowest_priced_orders.sql
+++ lowest_priced_orders.sql
@@ -2,3 +2,11 @@
 Find the lowest order cost of each customer.
 Output the customer id along with the first name and the lowest order price.
 */
+select c.id,
+        c.first_name,
+        MIN(total_order_cost) as min_order_cost
+from customers c
+join orders o
+on c.id = o.cust_id
+group by c.id, c.first_name
+order by c.id
